5487da77beccf28a2c24ee7b6a8deaa61af3b2ef
finanovita <finanovita479@gmail.com>
2026-04-15 10:40:56 +0700

    first commit

---
+++ src/test/java/com/tugas/deploy6/Deploy6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas.deploy6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
